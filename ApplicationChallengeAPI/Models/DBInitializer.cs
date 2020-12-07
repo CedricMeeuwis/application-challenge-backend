9135bfa9d76863e89acb71dcb07c47bb748a3a3d
Jasper Guldentops <r0760673@student.thomasmore.be>
2020-12-07 12:05:16 +0100

    Update initializer en wedstrijd FK's

--- ApplicationChallengeAPI/Models/DBInitializer.cs
+++ ApplicationChallengeAPI/Models/DBInitializer.cs
@@ -22,25 +22,26 @@ namespace ApplicationChallengeAPI.Models
               new Ploeg { Naam = "Jstack United", Bedrijfsnaam = "jstack", Locatie = "Kontich" },
               new Ploeg { Naam = "Grass City", Bedrijfsnaam = "Grasshoppers", Locatie = "Kontich" },
               new Ploeg { Naam = "QF", Bedrijfsnaam = "Qframe", Locatie = "Kontich" },
-              new Ploeg { Naam = "More Geel", Bedrijfsnaam = "Thomas more", Locatie = "Geel" }
+              new Ploeg { Naam = "More Geel", Bedrijfsnaam = "Thomas more", Locatie = "Geel" },
+              new Ploeg { Naam = "Testploeg", Bedrijfsnaam = "Testers", Locatie = "Testeren"}
             );
             context.SaveChanges();
 
             context.Users.AddRange(
                 //Kapiteinen
-                new User { Email = "aa@test.be", Naam = "Andre Andermans",  Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 1},
-                new User { Email = "bb@test.be", Naam = "Barry Barrens", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 2 },
-                new User { Email = "cc@test.be", Naam = "Connie Conners", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 3 },
-                new User { Email = "dd@test.be", Naam = "David Deckers", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 4 },
+                new User { Email = "aa@test.be", Naam = "Andre Andermans",  Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 1, IsKapitein = true },
+                new User { Email = "bb@test.be", Naam = "Barry Barrens", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 2, IsKapitein = true },
+                new User { Email = "cc@test.be", Naam = "Connie Conners", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 3, IsKapitein = true },
+                new User { Email = "dd@test.be", Naam = "David Deckers", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 4, IsKapitein = true },
                 //Gebruikers
                 new User { Email = "ee@test.be", Naam = "Erik Erens", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 1 },
                 new User { Email = "ff@test.be", Naam = "Femke Feyen", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 2 },
                 new User { Email = "gg@test.be", Naam = "Gerrie Gachternaam", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 3 },
                 new User { Email = "hh@test.be", Naam = "Harry Hekkens", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", PloegID = 4 },
                 //Testdata voor inloggen
-                new User { Email = "kapitein@test.be", Naam = "Kapitein Test", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test" },
-                new User { Email = "gebruiker@test.be", Naam = "Gebruiker Test", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test" },
-                new User { Email = "admin@test.be", Naam = "Admin Test", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test" }
+                new User { Email = "kapitein@test.be", Naam = "Kapitein Test", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test" , PloegID = 5, IsKapitein=true},
+                new User { Email = "gebruiker@test.be", Naam = "Gebruiker Test", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test"},
+                new User { Email = "admin@test.be", Naam = "Admin Test", Geboortedatum = DateTime.Now.AddDays(-7000), Passwoord = "test", IsAdmin = true}
 
             );
             context.SaveChanges();
@@ -50,16 +51,6 @@ namespace ApplicationChallengeAPI.Models
                 new Tafel { Naam = "Tafel TM", Bedrijfsnaam = "Thomas more", Adres = "Kleinhoefstraat 4, Geel", ContactpersoonID = 4 }
             );
             context.SaveChanges();
-
-            for (int i = 1; i <= 4; i++)
-            {
-                var ploeg = context.Ploegen.FirstOrDefault(p => p.PloegID == i);
-                var kapitein = context.Users.FirstOrDefault(u => u.PloegID == i);
-
-                ploeg.KapiteinID = kapitein.UserID;
-            }
-            context.SaveChanges();
-
         }
     }
 }
