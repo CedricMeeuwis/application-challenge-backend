5d8a850e4f2363afa6e48fa8e242dfef4f2bb664
Niel <r0758943@student.thomasmore.be>
2020-12-07 18:30:14 +0100

    users zonder passwoorden doorsturen, Putmethode voor users

--- ApplicationChallengeAPI/Controllers/UserController.cs
+++ ApplicationChallengeAPI/Controllers/UserController.cs
@@ -27,12 +27,25 @@ namespace ApplicationChallengeAPI.Controllers
             _context = context;
         }
 
-        [Authorize]
+        //[Authorize]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            var email = User.Claims.FirstOrDefault(c => c.Type == "Email").Value;
-            return await _context.Users.ToListAsync();
+            //var email = User.Claims.FirstOrDefault(c => c.Type == "Email").Value;
+
+            //alle users zonder passwoorden
+            return await _context.Users.Select(u => new User
+            {
+                UserID = u.UserID,
+                Naam = u.Naam,
+                Email = u.Email,
+                Foto = u.Foto,
+                Geboortedatum = u.Geboortedatum,
+                IsAdmin = u.IsAdmin,
+                IsKapitein = u.IsKapitein,
+                PloegID = u.PloegID,
+                Ploeg = u.Ploeg
+            }).ToListAsync();
         }
         // GET: api/User/Ploeg/1
         [HttpGet("Ploeg/{id}")]
@@ -72,6 +85,51 @@ namespace ApplicationChallengeAPI.Controllers
             await _context.SaveChangesAsync();
 
             return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<User>> PutUser(int id, User user)
+        {
+            if (id != user.UserID)
+            {
+                return BadRequest();
+            }
+
+            if (user.Passwoord != "")
+            {
+                byte[] salt;
+                new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+                var pbkdf2 = new Rfc2898DeriveBytes(user.Passwoord, salt, 100000);
+                byte[] hash = pbkdf2.GetBytes(20);
+
+                byte[] hashBytes = new byte[36];
+                Array.Copy(salt, 0, hashBytes, 0, 16);
+                Array.Copy(hash, 0, hashBytes, 16, 20);
+
+                string savedPasswordHash = Convert.ToBase64String(hashBytes);
+                user.Passwoord = savedPasswordHash;
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return user;
         }
 
         [HttpPost("authenticate")]
@@ -118,6 +176,11 @@ namespace ApplicationChallengeAPI.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.UserID == id);
         }
     }
 }
