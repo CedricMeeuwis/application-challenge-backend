f71438aa4dc1535c2e5951b0da4211a8ad8dbd13
unknown <r0740785@student.thomasmore.be>
2020-12-09 16:56:15 +0100

    fix ploeg bugs en toevoegen competities

--- ApplicationChallengeAPI/Models/Competitie.cs
+++ ApplicationChallengeAPI/Models/Competitie.cs
@@ -9,6 +9,7 @@ namespace ApplicationChallengeAPI.Models
     public class Competitie
     {
         public int CompetitieID { get; set; }
+        public string Naam { get; set; }
         public string Periode { get; set; }
         public int ParticipentAantal { get; set; }//1 = 1v1; 2 = 2v2
 
